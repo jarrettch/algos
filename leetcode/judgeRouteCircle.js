8e19f303ae769cf7854698aa6b54fb97c0234632
jarrettch <jarrett.harris@gmail.com>
2017-08-14 21:23:48 -0700

    Wrong answer that was accepted

--- leetcode/judgeRouteCircle.js
+++ leetcode/judgeRouteCircle.js
@@ -2,24 +2,29 @@
  * @param {string} moves
  * @return {boolean}
  */
-// const moves = "UD";
 
-var judgeCircle = function(moves) {
-  let count = 0;
-  // console.log(moves);
-  for (let i = 0; i < moves.length; i++) {
-    if (moves[i] === "U" || moves[i] === "R") {
-      count++;
-    } else {
-      count--;
-    }
-  };
-  // console.log(count);
-  if (count === 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
+//WRONG ANSWER BUT ACCEPTED
+
+const moves = "URRLDD";
+
+// var judgeCircle = function(moves) {
+//   let count = 0;
+//   // console.log(moves);
+//   for (let i = 0; i < moves.length; i++) {
+//     if (moves[i] === "U" || moves[i] === "R") {
+//       count++;
+//     } else {
+//       count--;
+//     }
+//   };
+//   // console.log(count);
+//   if (count === 0) {
+//     console.log('true');
+//     return true;
+//   } else {
+//     console.log('false');
+//     return false;
+//   }
+// };
 
 judgeCircle(moves);
